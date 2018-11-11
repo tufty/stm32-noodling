8eb905650dd68e1366f2f48c466f23a42de58766
Simon Stapleton <simon.stapleton@gmail.com>
2018-11-11 11:02:07 +0100

    Adding some utilities

---
+++ Core/Inc/util.h
@@ -0,0 +1,20 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__ 1
+
+#include <stm32f1xx_hal.h>
+#include "main.h"
+
+#define LED_ON()  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 0)
+#define LED_OFF() HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 1)
+
+// "Binary morse" signalling of a value
+void morse(uint32_t value, uint32_t bits);
+
+#define morse8(x)  morse(x, 8)
+#define morse16(x) morse(x, 16)
+#define morse32(x) morse(x, 32)
+
+int puts (const char * src);
+size_t strlen(const char * src);
+
+#endif
---
+++ Core/Src/util.c
@@ -0,0 +1,31 @@
+/* Utility code */
+
+#include "util.h"
+#include "usart.h"
+
+// Flash a value in "binary morse"
+void morse(uint32_t value, uint32_t bits) {
+  for (int i = 0; i < bits; i++) {
+    LED_OFF();
+    if (((value << i) & (1 << (bits - 1)))) {
+      HAL_Delay(100);
+      LED_ON();
+      HAL_Delay(400);
+    } else {
+      HAL_Delay(400);
+      LED_ON();
+      HAL_Delay(100);
+    }
+  }
+  LED_OFF();
+}
+
+size_t strlen(const char * src) {
+  char * c = (char *)src;
+  for (; c != 0; c++) {}
+  return (size_t)(c - src);
+}
+
+int puts(const char * src) {
+  return HAL_UART_Transmit(&huart1, (uint8_t *)src, strlen(src), 100);
+}
